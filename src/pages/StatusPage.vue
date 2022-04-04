e4b95f3da0860af72e8264fe8e8d306aab75d40f
fuzzbear (bot dev) <58638135+fuzzbear0001@users.noreply.github.com>
2022-04-04 16:19:22 +0100

    Update StatusPage.vue

--- src/pages/StatusPage.vue
+++ src/pages/StatusPage.vue
@@ -227,7 +227,7 @@
             </div>
 
             <footer class="mt-5 mb-4">
-                {{ $t("Powered by") }} <a target="_blank" href="https://github.com/louislam/uptime-kuma">{{ $t("Uptime Kuma" ) }}</a>
+                {{ $t("Powered by") }} <a target="_blank" href="https://frogy.xyz">{{ $t("Frogy.xyz" ) }}</a>
             </footer>
         </div>
 
